dd38a2ef945be4ef09780d5e885f24c6180907a8
John MacKintosh <johnmackintosh@users.noreply.github.com>
2017-08-08 22:47:15 +0100

    Update yearlyweather.R
    added roxygen skeleton

--- yearlyweather.R
+++ yearlyweather.R
@@ -1,6 +1,18 @@
 
+#' yearlyweather
+#'
+#' @param weatheryear = the year to process
+#' @param station - the weather station to get data from, defaults to Inverness Airport
+#'
+#' @return
+#' @export
+#'
+#' @examples
 yearlyweather <- function(weatheryear,station = NULL) {
   getSummarizedWeather(station,start_date = paste0(weatheryear,"-01-01"),
                        end_date = paste0(weatheryear, "-12-31"),
                        opt_all_columns = TRUE)
 }
+
+
+
